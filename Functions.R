30027f4af505abe0a51d522a8f6761dbd58877d6
Gabriel Muñoz <fgabriel1891@gmail.com>
2020-03-04 11:42:48 -0500

    Update Functions.R

--- Functions.R
+++ Functions.R
@@ -2,7 +2,7 @@
 
 # https://www.r-bloggers.com/extracting-colours-from-your-images-with-image-quantization/
 
-get_colorPal <- function(im, n=8, cs="RGB"){
+get_colorPal <- function(im, n=n, cs=cs){
   #print(cs) 
   tmp <-im %>% image_resize("100") %>% 
     image_quantize(max=n, colorspace=cs) %>%  ## reducing colours! different colorspace gives you different result
@@ -18,4 +18,30 @@ get_colorPal <- function(im, n=8, cs="RGB"){
   
   return(tmp %>% select(colorspace,hex,hue,sat,value,n)) ## I want data frame as a result.
   
+}
+
+
+#####
+# Wrapper function to extract the color from arthropod figures 
+
+getColorOdonate <- function(path, n, max,cs, quantile){
+  ## I'll use this plum flower image I took while back to extract colour. 
+  ## using image_read function in magick I can read image as below. 
+  im <- image_read(path)
+  
+  ## now display image with 500px wide
+  im %>% image_resize("500")
+  ## Reduce the colour used in image with image_quantize.  For example, let's say I want to reduce to 24 colours.
+  im %>%
+    image_resize("500") %>%
+    image_quantize(max=max)
+  
+  get_colorPal(im, n, cs)
+  params <- list(im=list(im), 
+                 n=n, ## number of colour you want 
+                 cs=colorspace_types()[-5]) ## gray fails so I've removed it...
+  
+  my_colors <- pmap_df(params,get_colorPal)
+  
+  my_colors <- my_colors[(1-(my_colors$n/max(my_colors$n))) > quantile,]
 }
