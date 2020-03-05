59a28f9f74a335eb78ec332d692b889996ab8889
Gabriel Muñoz <fgabriel1891@gmail.com>
2020-03-05 16:03:02 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -6,23 +6,39 @@ library(scales) ## I find rescale function so useful!  and i love show_col funct
 library(imager) ## i don't know how else to convert image to data frame at the moment. 
 library(colorspace)
 
-navbarPage("Odonate Color Extractor",
-           tabPanel("Explore the color space",fluidPage(theme = shinytheme("flatly")),
+navbarPage("ColorExtractR",
+           tabPanel("1: Explore the color space",fluidPage(theme = shinytheme("yeti")),
                     tags$head(
                       tags$style(HTML(".shiny-output-error-validation{color: red;}"))),
                     pageWithSidebar(
-                      headerPanel('Parameters'),
+                      headerPanel('Settings'),
                       sidebarPanel(width = 4,
                                    fileInput("picture", label = "Upload the scan here", multiple = F),
+                                   p("Control this slider to set the number of initial distinct colors to separate"),
                                    sliderInput("n", "Number of colors:",
                                                min = 1, max = 100,
                                                value = c(10)),
+                                   p("Control this slider to set the number of defined color groups to create from the sampled colors"),
                                    sliderInput("max", "Maximum number of colors to quantize:",
                                                min = 1, max = 100,
                                                value = c(10)),
+                                   p("Control this slider to remove colors based on their percentile"),
                                    sliderInput("percentile", "Percentile to remove:",
                                                min = 0, max = 1,
                                                value = c(0.5)),
+                                   p("Control this slider to select portions of the HUE space"),
+                                   sliderInput("HUE", "Select hue range",
+                                               min = 0, max = 360,
+                                               value = c(0,360)),
+                                   p("Control this slider to select portions of the Brigthness space"),
+                                   sliderInput("BRI", "Select brightness range",
+                                               min = 0, max = 1,
+                                               value = c(0,0.5)),
+                                   p("Control this slider to select portions of the saturation space"),
+                                   sliderInput("SAT", "Select saturation range",
+                                               min = 0, max = 1,
+                                               value = c(0,0.5)),
+                                   p("Select the color space to analize"),
                                    checkboxGroupInput(inputId = "colorspace",
                                                       label = 'ColorSpace:', choices = c("RGB" = "RGB"),
                                                       selected = c("RGB"="RGB"),inline=TRUE),
@@ -45,9 +61,9 @@ navbarPage("Odonate Color Extractor",
                         )
                         )
                     ),
-           tabPanel("Explore differences in coloration",
+           tabPanel("2: Batch explore coloration differences",
                     pageWithSidebar(
-                      headerPanel('Parameters'),
+                      headerPanel('Settings'),
                       sidebarPanel(width = 4,
                                    fileInput("picture2", label = "Upload the standardized 
                                              scans here", multiple = T),
@@ -60,6 +76,15 @@ navbarPage("Odonate Color Extractor",
                                    sliderInput("percentile2", "Percentile to remove:",
                                                min = 0, max = 1,
                                                value = c(0.5)),
+                                   sliderInput("HUE2", " Select HUE range:",
+                                               min = 0, max = 360,
+                                               value = c(0,360)),
+                                   sliderInput("BRI2", "Select brightness range",
+                                               min = 0, max = 1,
+                                               value = c(0,1)),
+                                   sliderInput("SAT2", "Select saturation range",
+                                               min = 0, max = 1,
+                                               value = c(0,1)),
                                    checkboxGroupInput(inputId = "colorspace2",
                                                       label = 'ColorSpace:', choices = c("RGB" = "RGB"),
                                                       selected = c("RGB"="RGB"),
@@ -82,12 +107,33 @@ navbarPage("Odonate Color Extractor",
                       )
                     )),
            tabPanel("About",
-                    p(), ".",style = "font-size:25px"),
+                    pageWithSidebar(
+                        tags$img(src = "imgfile.png"),
+                      sidebarPanel(
+                        h1("Welcome to ColorExtractR"))
+                      ,
+                      mainPanel(
+                        
+                        p(),
+                        p(),p(),
+                        p("This web application was created with the objective to extract and quantify the variation in  coloration schemes 
+                          of digitized arthropod images. Nevertheless, it might very well be suitable for simple color analysis using any picture as input"),
+                        p(),p(),p(),
+                        p("Find the repository:"),p(),
+                        p(a("https://github.com/fgabriel1891/ArthropodColorExtract",
+                            href="", target="https://github.com/fgabriel1891/ArthropodColorExtract"))
+                        
+                        
+                        
+                      )
+                    ),
                     
-                    hr(), 
+                  
+                    p(), ".",style = "font-size:25px"),
            
            tabPanel("Developer",
-                    p(a("Gabriel Muñoz", href="", target="_blank"),style = "font-size:25px"),
-                    p("e-mail: gabriel.munoz@concordia.ca",style = "font-size:20px"))
+                    p(a("Gabriel Muñoz", href="", target="https://github.com/fgabriel1891/"),style = "font-size:25px"),
+                    p("e-mail: gabriel.munoz@concordia.ca",style = "font-size:20px"),
+                    p("gitHub: /fgabriel1891",style = "font-size:20px"))
 )
 
